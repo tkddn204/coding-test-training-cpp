1a7e97e1228d2a2605a06aa135b7db5be4068abf
rightpair <tkddn204@gmail.com>
2023-08-05 19:36:48 +0900

    update template

--- template.cpp
+++ template.cpp
@@ -8,11 +8,12 @@ void solve() {
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
 #ifndef ONLINE_JUDGE
   freopen("input.txt", "r", stdin);
   freopen("output.txt", "w", stdout);
+#else
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
 #endif
   solve();
   return 0;
